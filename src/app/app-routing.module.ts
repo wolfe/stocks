4b3861af2179b7dc094655b443ba874bfa50232c
David Wolfe <davidgameswolfe@gmail.com>
2021-10-25 12:21:46 -0300

    Added route /stocks with nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StocksComponent } from './stocks/stocks.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'stocks', component: StocksComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
